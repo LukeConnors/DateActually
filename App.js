d8555240cd400af1b25812bf7cd90773bb8c6c69
unknown <luketconnors@yahoo.com>
2024-03-26 14:03:01 -0500

    learning how to use image component/import

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   console.log("Hello world! Success!");
@@ -8,7 +8,16 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress} style={styles.title}>DATE ACTUALLY - we can say a lot of things because we want to test the number of lines truncation feature on the text element.</Text>
+      <Text numberOfLines={3} onPress={handlePress} style={styles.title}>DATE ACTUALLY - we can say a lot of things because we want to test the number of lines truncation feature on the text element.</Text>
+     {/* require  returns reference number to image */}
+     {/* <Image source={require('./assets/icon.png')} /> */}
+     {/* Network images require specified dimensions to be visible,
+      while local images do not. */}
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}
+        } />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -17,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
