8334e8a9a65dbfc37f059c15bf8f7d34fca48e41
unknown <luketconnors@yahoo.com>
2024-04-15 12:08:02 -0500

    using prebuilt functions through hooks

--- App.js
+++ App.js
@@ -11,10 +11,11 @@ import {
   TouchableNativeFeedback,
   Dimensions
 } from 'react-native';
-import {useDimensions} from '@react-native-community/hooks'
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
 
 export default function App() {
-  console.log(useDimensions)
+  console.log(useDeviceOrientation())
+  const {landscape} = useDeviceOrientation();
 
   const handlePress = () => console.log("Text clicked!");
 
@@ -23,7 +24,7 @@ export default function App() {
     style={{
       backgroundColor: "red",
       width: "100%",
-      height: "30%",
+      height: landscape ? "100%" : "30%",
     }}
     >
 
