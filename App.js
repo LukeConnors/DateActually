b436446d3ce9fc1b17cd8973b5e76e64cfcad0d8
unknown <luketconnors@yahoo.com>
2024-04-29 16:28:17 -0500

    same thing

--- App.js
+++ App.js
@@ -25,10 +25,10 @@ export default function App() {
     flexWrap: "wrap"
   }}>
  <View style={{
-  backgroundColor: "red",
+  backgroundColor: "black",
   flexBasis: 100, //flex basis can set width or height
-  flexGrow: 1,
-  // width: 100,
+  flex: -1,
+  width: 400,
   height: 100,
  }}>
  </View>
@@ -36,24 +36,16 @@ export default function App() {
   backgroundColor: "dodgerblue",
   width: 100,
   height: 100,
+  right: 20,
+  top: 20, 
+  position: "absolute",
 }}>
 </View>
 <View style={{
   backgroundColor: "green",
   width: 100,
   height: 100,
-}}>
-</View>
-<View style={{
-  backgroundColor: "yellow",
-  width: 100,
-  height: 100,
-}}>
-</View>
-<View style={{
-  backgroundColor: "gray",
-  width: 100,
-  height: 100,
+  top: 20,
 }}>
 </View>
   </View>
