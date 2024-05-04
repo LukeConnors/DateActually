5bf19fa7027dc440434af4ccc52a367827deb721
unknown <luketconnors@yahoo.com>
2024-05-04 17:34:11 -0500

    styling

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View} from 'react-native';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
         style = {styles.background}
         source={require('../assets/romance.jpg')}
-        ></ImageBackground>
-    );
 
+        >
+            <View style={styles.loginButton}>
+
+            </View>
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
 background: {
+flex: 1,
+},
 
+loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "black",
 }
 })
 
